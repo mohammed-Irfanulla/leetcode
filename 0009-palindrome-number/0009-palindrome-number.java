e0f8e2c48683cd054022bc8485f46bcd2c2d11ca
mohammed-Irfanulla <mohammedirfanulla76@gmail.com>
2026-07-27 22:53:41 +0530

    Time: 5 ms (80.8%), Space: 45.7 MB (87.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,8 +4,9 @@ class Solution {
             return false;
         int rev=0;
         int num=x;
+        int rem=0;
         while ( x!= 0 ){
-            int rem = x%10;
+            rem = x%10;
             rev = rev *10 + rem;
             x = x/10;
         }
